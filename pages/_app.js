12b0e6502d20a3b8c2ba429b2da5458f3263410e
anykord <101556060+anykord@users.noreply.github.com>
2025-10-26 12:25:24 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
+// pages/_app.js
 import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
+import "@/styles/globals.css";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
+
