bbe2e12ea7b22b36962d6b8093d739c49514c433
anykord <101556060+anykord@users.noreply.github.com>
2025-10-26 10:21:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Если ESLint ругается — не валим билд
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // На всякий случай (мы TS не используем, но если появятся типы — не ломаем сборку)
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 module.exports = nextConfig;
