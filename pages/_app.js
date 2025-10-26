fb94e1ce5d0733322325e93ba46666ae5ae7d55a
anykord <101556060+anykord@users.noreply.github.com>
2025-10-26 12:59:35 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 // pages/_app.js
 import { SessionProvider } from "next-auth/react";
-import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
-
