2bed4b9fa7c03261e2528521d63406f6c438e8d7
anykord <101556060+anykord@users.noreply.github.com>
2025-10-27 12:36:24 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
-// middleware.js — защищаем только /dashboard
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard/:path*"],
 };
