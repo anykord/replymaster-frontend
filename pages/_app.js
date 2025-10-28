c607f6b5ec3565a29f90331554cb84a8f3154c9a
anykord <101556060+anykord@users.noreply.github.com>
2025-10-28 12:05:41 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,16 @@
-import Head from 'next/head'
-import '../styles/globals.css'
-import { useRouter } from 'next/router'
-import DashboardLayout from '../components/layouts/DashboardLayout'
+// pages/_app.js
+import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  const isDashboard = router.pathname === '/dashboard' || router.pathname.startsWith('/dashboard/')
-
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         <title>Replymaster</title>
       </Head>
-      {isDashboard ? (
-        <DashboardLayout>
-          <Component {...pageProps} />
-        </DashboardLayout>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </>
-  )
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
